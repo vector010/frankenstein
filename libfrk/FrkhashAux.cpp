a4c740d91921e32f49f29992bc551817f281e863
Julian <jrodriquez@publicgalaxy.com>
2021-07-05 13:56:21 -0500

    Stuipid test

--- libfrk/FrkhashAux.cpp
+++ libfrk/FrkhashAux.cpp
@@ -17,8 +17,8 @@ using namespace exp;
 
 Result FrkhashAux::eval(h256 const& _headerHash, uint64_t _nonce) noexcept {
     auto headerHash = frkhash::hash256_from_bytes(_headerHash.data());
-    auto result = frkhash::frkhash_hash(headerHash, _nonce);
-    h256 mix{reinterpret_cast<byte*>(result.mix_hash.bytes), h256::ConstructFromPointer};
-    h256 final{reinterpret_cast<byte*>(result.final_hash.bytes), h256::ConstructFromPointer};
-    return {final, mix};
+    //auto result = frkhash::hash(headerHash, _nonce);
+    //h256 mix{reinterpret_cast<byte*>(result.mix_hash.bytes), h256::ConstructFromPointer};
+    //h256 final{reinterpret_cast<byte*>(result.final_hash.bytes), h256::ConstructFromPointer};
+    return frkhash::hash(headerHash, _nonce);
 }
